38098776913bf4ac194e4faa6859f723de5af2bc
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-02-05 21:30:46 +0100

    Initialised variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useCallback, useRef, useEffect } from "react";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App: React.FC = () => {
+  const numRows = 50;
+  const numCols = 70;
+  const cellSize = 12;
 
-export default App
+   const [grid, setGrid] = useState<number[][]>(() => 
+    Array(numRows).fill(null).map(() => Array(numCols).fill(0))
+  );
+
+  const [running, setRunning] = useState<boolean>(false);
+  const [generation, setGeneration] = useState<number>(0);
+  const [speed, setSpeed] = useState<number>(100);
+  const [drawMode, setDrawMode] = useState<boolean>(false);
+  const [rainbow, setRainbow] = useState<boolean>(false);
+
+  const runningRef = useRef<boolean>(running);
+  runningRef.current = running;
+
+  const speedRef = useRef<number>(speed);
+  speedRef.current = speed;
+
+  // Neighbor positions
+  const operations: number[][] = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+  // Logic
+
+  const runSimulation = useCallback(() => {
+    
+  })
+  return <div>App</div>;
+};
+
+export default App;
